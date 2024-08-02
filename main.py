407dfda547d4d645acf1f0697aa0678d4c58cfab
paskozdilar <paskozdilar@gmail.com>
2024-08-02 16:09:44 +0200

    Add cert check

--- main.py
+++ main.py
@@ -21,6 +21,15 @@ def main():
     else:
         print('ok')
 
+    print()
+
+    try:
+        test_client_no_certificate()
+    except Exception as exc:
+        print('error:', exc)
+    else:
+        print('ok')
+
 
 def test_client_valid():
     """ Connect to redis using valid server-signed credentials """
@@ -42,7 +51,7 @@ def test_client_valid():
 
 
 def test_client_self_signed():
-    """ Connect to redis using valid server-signed credentials """
+    """ Connect to redis using self-signed credentials """
 
     print("test client self-signed")
     r = redis.Redis(
@@ -53,6 +62,23 @@ def test_client_self_signed():
         ssl_ca_certs='certs/ca-cert.pem',
         ssl_certfile='certs/self-signed-client-cert.pem',
         ssl_keyfile='certs/self-signed-client-key.pem',
+    )
+    d = random.randbytes(1024)
+    r.set(b'key', d)
+    v = r.get('key')
+    assert v == d
+
+
+def test_client_no_certificate():
+    """ Connect to redis without client certificate """
+
+    print("test client no certificate")
+    r = redis.Redis(
+        host='localhost',
+        port=6379,
+
+        ssl=True,
+        ssl_ca_certs='certs/ca-cert.pem',
     )
     d = random.randbytes(1024)
     r.set(b'key', d)
